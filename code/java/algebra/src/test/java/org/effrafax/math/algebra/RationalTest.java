6a814178925e0b499a14211f552fac5e5745e272
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2009-07-28 18:29:12 +0000

    Expanded the tests for the Rationals Field.
    It caught some errors in the code which are now fixed.

--- code/java/algebra/src/test/java/org/effrafax/math/algebra/RationalTest.java
+++ code/java/algebra/src/test/java/org/effrafax/math/algebra/RationalTest.java
@@ -14,33 +14,141 @@ import org.junit.Test;
  */
 public class RationalTest {
 
+	private Rational zero = new Rational(0, 1);
+	private Rational one = new Rational(1, 1);
+	private Rational two = new Rational(2, 1);
+	private Rational minusTwo = new Rational(-2, 1);
+	private Rational aHalf = new Rational(1, 2);
+
 	@Test
-	public void testZeroRational() {
+	public void testZeroIsAnInteger() {
 
-		Rational element = new Rational(0, 1);
-
-		assertTrue(element.isZero());
-		assertFalse(element.isOne());
-		assertTrue(element.isInteger());
+		assertTrue(zero.isInteger());
 	}
 
 	@Test
-	public void testOneRational() {
+	public void testZeroIsZero() {
 
-		Rational element = new Rational(1, 1);
+		assertTrue(zero.isZero());
+	}
 
-		assertFalse(element.isZero());
-		assertTrue(element.isOne());
-		assertTrue(element.isInteger());
+	@Test
+	public void testZeroIsNotOne() {
+
+		assertFalse(zero.isOne());
+	}
+
+	@Test
+	public void testOneIsAnInteger() {
+
+		assertTrue(one.isInteger());
+	}
+
+	@Test
+	public void testOneIsNotZero() {
+
+		assertFalse(one.isZero());
+	}
+
+	@Test
+	public void testOneIsOne() {
+
+		assertTrue(one.isOne());
+	}
+
+	@Test
+	public void testZeroPlusZeroEqualsZero() {
+
+		assertEquals(zero, zero.add(zero));
 	}
 
 	@Test
 	public void testOnePlusZeroEqualsOne() {
 
-		Rational zero = new Rational(0, 1);
-		Rational one = new Rational(1, 1);
-
 		assertEquals(one, zero.add(one));
 		assertEquals(one, one.add(zero));
+	}
+
+	@Test
+	public void testOneTimesOneEqualsOne() {
+
+		assertEquals(one, one.multiply(one));
+	}
+
+	@Test
+	public void testOneTimesZeroEqualsZero() {
+
+		assertEquals(zero, one.multiply(zero));
+		assertEquals(zero, zero.multiply(one));
+	}
+
+	@Test
+	public void testZeroTimesZeroEqualsZero() {
+
+		assertEquals(zero, zero.multiply(zero));
+	}
+
+	@Test
+	public void testTwoIsAnInteger() {
+
+		assertTrue(two.isInteger());
+	}
+
+	@Test
+	public void testMinusTwoHasANegativeNumerator() {
+
+		assertTrue(minusTwo.getNumerator() < 0);
+	}
+
+	@Test
+	public void testMinusTwoHasAPositiveDenominator() {
+
+		assertTrue(minusTwo.getDenominator() > 0);
+	}
+
+	@Test
+	public void testMinusTwoIsAnInteger() {
+
+		assertTrue(minusTwo.isInteger());
+	}
+
+	@Test
+	public void testAHalfIsNotAnInteger() {
+
+		assertFalse(aHalf.isInteger());
+	}
+
+	@Test
+	public void testMinusTwoIsTheNegativeOfTwo() {
+
+		assertEquals(minusTwo, two.negative());
+		assertEquals(two, minusTwo.negative());
+	}
+
+	@Test
+	public void testTwoPlusMinusTwoEqualsZero() {
+
+		assertEquals(zero, two.add(minusTwo));
+		assertEquals(zero, minusTwo.add(two));
+	}
+
+	@Test
+	public void testTwoSubtractedTwoEqualsTwo() {
+
+		assertEquals(zero, two.subtract(two));
+	}
+
+	@Test
+	public void testOneHalfIsTheInverseOfTwo() {
+
+		assertEquals(two, aHalf.inverse());
+		assertEquals(aHalf, two.inverse());
+	}
+
+	@Test
+	public void testTwoTimesAHalfEqualsOne() {
+
+		assertEquals(one, two.multiply(aHalf));
+		assertEquals(one, aHalf.multiply(two));
 	}
 }
