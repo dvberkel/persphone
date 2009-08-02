868a5ce17186f80b60d1cd01bd73337807a0c2e8
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2009-08-02 15:11:48 +0000

    Finished countUnlockPattern.
    This code is finished and reproduces computations made with a different
    implementation.

--- trunk/code/python/unlock-patterns/countUnlockPattern.py
+++ trunk/code/python/unlock-patterns/countUnlockPattern.py
@@ -1,18 +1,64 @@
 #! /usr/bin/env python
 
+def gcd(a,b):
+	if a == 0:
+		return b
+	elif b == 0:
+		return a
+	else:
+		return gcd(b, a % b)
+
+def sign(a):
+	if a >= 0:
+		return 1
+	else:
+		return -1
+
 class Pair:
 	x = 0
 	y = 0
 	
 	def __init__(self, x, y):
 		self.x = x
-		self.y = y	
+		self.y = y
 	
 	def translate(self, dx, dy):
 		return Pair(self.x + dx, self.y + dy)
 	
+	def __eq__(self, other):
+		if (other == None):
+			return False
+		
+		return self.x == other.x and self.y == other.y
+	
+	def __hash__(self):
+		prime = 37
+		
+		result = 1
+		result = prime * result + self.x
+		result = prime * result + self.y
+		
+		return result		
+	
 	def __str__(self):
 		return "(" + str(self.x) + "," + str(self.y) + ")"
+	
+	def pairsInBetween(self, target):
+		dx = target.x - self.x
+		dy = target.y - self.y
+		
+		d = gcd(abs(dx),abs(dy));
+		
+		dx = sign(dx) * (abs(dx) / d)
+		dy = sign(dy) * (abs(dy) / d)
+			
+		inBetween = set()
+		pair = self.translate(dx, dy)
+		while not pair == target:
+			inBetween.add(pair)
+			pair = pair.translate(dx,dy)
+		
+		return inBetween
 
 class PatternTree:
 	parent = None
@@ -38,7 +84,12 @@ class PatternTree:
 				 
 	
 	def reachable(self, target):
-		return True
+		if self.patternElement == None:
+			return True
+		else:
+			obstructions = self.patternElement.pairsInBetween(target)
+			
+			return len(self.possible.intersection(obstructions)) == 0
 		
 	def depth(self):
 		if self.parent != None:
@@ -76,8 +127,13 @@ if __name__ == "__main__":
 	possible = set()
 	possible.add(Pair(0,0))
 	possible.add(Pair(1,0))
+	possible.add(Pair(2,0))
+	possible.add(Pair(2,1))
 	possible.add(Pair(1,1))
 	possible.add(Pair(0,1))
+	possible.add(Pair(0,2))
+	possible.add(Pair(1,2))
+	possible.add(Pair(2,2))
 	
 	unlockPatterns = PatternTree(None, None, possible)
 	unlockPatterns.generate()
