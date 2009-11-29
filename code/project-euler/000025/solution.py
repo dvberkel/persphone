417f0a9f343c94e7b29e20b2827ddd78f20bf2a0
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2009-11-29 08:58:18 +0000

    Created Solutions
    Created solutions for problems 24 and 25.

--- code/project-euler/000025/solution.py
+++ code/project-euler/000025/solution.py
@@ -1,3 +1,21 @@
 #! /usr/bin/env python
 
+from math import log
+
+def numberOfDigits(n):
+	return int(log(n) / log(10) + 1)
+
 if __name__ == '__main__':
+	bound = 1000
+	
+	f_old = 1
+	f_new = 1
+	
+	count = 2
+	while (numberOfDigits(f_new) < bound):
+		(f_old, f_new) = (f_new, f_old + f_new)
+		count = count + 1
+	
+	print count
+	
+	
