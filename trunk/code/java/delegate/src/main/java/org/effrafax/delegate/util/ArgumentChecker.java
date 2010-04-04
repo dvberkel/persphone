5e1530f445e2ffa26b6e1047fb53a5464959e4b7
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-04-04 08:32:33 +0000

    Added ArgumentChecker
    ArgumentChecker and accompaning tests where added.

---
+++ trunk/code/java/delegate/src/main/java/org/effrafax/delegate/util/ArgumentChecker.java
@@ -0,0 +1,26 @@
+package org.effrafax.delegate.util;
+
+/**
+ * @author dvberkel
+ */
+public class ArgumentChecker {
+
+	public static void checkIfArgumentsAreNotNull(Object... arguments) {
+
+		for (Object argument : arguments) {
+			throwExceptionIfArgumentIsNull(argument);
+		}
+	}
+
+	private static void throwExceptionIfArgumentIsNull(Object argument) {
+
+		if (isNull(argument)) {
+			throw new IllegalArgumentException(String.format("Argument should not be null"));
+		}
+	}
+
+	public static boolean isNull(Object argument) {
+
+		return argument == null;
+	}
+}
