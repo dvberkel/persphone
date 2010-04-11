d68c6c50d4b1a3966161cbfaf133a5445423403b
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-04-11 15:59:26 +0000

    Created an Album
    An album and an implementation is created.

---
+++ trunk/code/java/comiccollection/domain/src/main/java/org/effrafax/comiccollection/domain/model/Album.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.effrafax.comiccollection.domain.model;
+
+import org.effrafax.comiccollection.domain.model.interfaces.Indexable;
+import org.effrafax.comiccollection.domain.model.interfaces.Nameable;
+
+/**
+ * This interface represents one comic album.
+ * 
+ * @author dvberkel
+ */
+public interface Album extends Nameable, Indexable {
+
+}
---
+++ trunk/code/java/comiccollection/domain/src/main/java/org/effrafax/comiccollection/domain/model/implementation/SimpleAlbum.java
@@ -0,0 +1,112 @@
+/**
+ * 
+ */
+package org.effrafax.comiccollection.domain.model.implementation;
+
+import org.effrafax.comiccollection.domain.model.Album;
+import org.effrafax.comiccollection.domain.model.interfaces.Indexable;
+import org.effrafax.comiccollection.domain.model.interfaces.Nameable;
+import org.effrafax.comiccollection.domain.model.interfaces.implementation.SimpleIndexable;
+import org.effrafax.comiccollection.domain.model.interfaces.implementation.SimpleNameable;
+import org.effrafax.comiccollection.util.ArgumentChecker;
+
+/**
+ * @author dvberkel
+ */
+public class SimpleAlbum implements Album {
+
+	/**
+	 * Delegate for the {@link Indexable} interface.
+	 */
+	private Indexable indexableDelegate;
+
+	/**
+	 * Delegate for the {@link Nameable} interface.
+	 */
+	private Nameable nameableDelegate;
+
+	/**
+	 * Constructor for this {@link SimpleAlbum}
+	 * 
+	 * @param index
+	 *            the index of this {@link SimpleAlbum}.
+	 * @param name
+	 *            the name of this {@link SimpleAlbum}.
+	 */
+	public SimpleAlbum(Integer index, String name) {
+
+		setIndexableDelegate(new SimpleIndexable(index));
+		setNameableDelegate(new SimpleNameable(name));
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see org.effrafax.comiccollection.domain.model.interfaces.Nameable#getName()
+	 */
+	@Override
+	public String getName() {
+
+		return getNameableDelegate().getName();
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see org.effrafax.comiccollection.domain.model.interfaces.Indexable#getIndex()
+	 */
+	@Override
+	public Integer getIndex() {
+
+		return getIndexableDelegate().getIndex();
+	}
+
+	/**
+	 * Getter for the {@link Nameable} delegate.
+	 * 
+	 * @return the {@link Nameable} delegate.
+	 */
+	private Nameable getNameableDelegate() {
+
+		return nameableDelegate;
+	}
+
+	/**
+	 * Setter for the {@link Nameable} delegate.
+	 * 
+	 * @param nameableDelegate
+	 *            a {@link Nameable} delegate.
+	 * @throws IllegalArgumentException
+	 *             if {@code nameableDelegate} is null.
+	 */
+	private void setNameableDelegate(Nameable nameableDelegate) {
+
+		ArgumentChecker.throwExceptionIfAnyArgumentIsNull(nameableDelegate);
+		this.nameableDelegate = nameableDelegate;
+	}
+
+	/**
+	 * Getter for the {@link Indexable} delegate.
+	 * 
+	 * @return the {@link Indexable} delegate.
+	 */
+	private Indexable getIndexableDelegate() {
+
+		return indexableDelegate;
+	}
+
+	/**
+	 * Setter for the {@link Indexable} delegate.
+	 * 
+	 * @param indexableDelegate
+	 *            a {@link Indexable} delegate.
+	 * @throws IllegalArgumentException
+	 *             if {@code indexableDelegate} is null.
+	 */
+	private void setIndexableDelegate(Indexable indexableDelegate) {
+
+		ArgumentChecker.throwExceptionIfAnyArgumentIsNull(indexableDelegate);
+		this.indexableDelegate = indexableDelegate;
+	}
+
+}
