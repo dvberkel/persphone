1ef808df1556a650a2b35abfb73668d840c5b040
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-04-11 15:09:30 +0000

    Created indexable interface.
    the indexable interface is created.

---
+++ code/java/comiccollection/domain/src/main/java/org/effrafax/comiccollection/domain/model/interfaces/Indexable.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.effrafax.comiccollection.domain.model.interfaces;
+
+/**
+ * This class represenant the possibility to index a certain object.
+ * 
+ * @author dvberkel
+ */
+public interface Indexable {
+
+	/**
+	 * Returns the index associated with this object.
+	 * 
+	 * @return the index of this object.
+	 */
+	public Integer getIndex();
+}
