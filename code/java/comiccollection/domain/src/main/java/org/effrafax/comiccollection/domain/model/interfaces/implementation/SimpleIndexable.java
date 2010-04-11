eb90d2872d76e85bc6ed0910421031e563e87cb3
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-04-11 15:17:52 +0000

    Created an Implementation
    The Indexable interface has a simple implementation.

---
+++ code/java/comiccollection/domain/src/main/java/org/effrafax/comiccollection/domain/model/interfaces/implementation/SimpleIndexable.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package org.effrafax.comiccollection.domain.model.interfaces.implementation;
+
+import org.effrafax.comiccollection.domain.model.interfaces.Indexable;
+
+/**
+ * @author dvberkel
+ */
+public class SimpleIndexable implements Indexable {
+
+	/**
+	 * This field will hold the index associated with this
+	 * {@link SimpleIndexable}.
+	 */
+	private Integer index;
+
+	/**
+	 * The constructor for this {@link SimpleIndexable}.
+	 * 
+	 * @param index
+	 *            the index associated with this {@link SimpleIndexable}
+	 */
+	public SimpleIndexable(Integer index) {
+
+		setIndex(index);
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see org.effrafax.comiccollection.domain.model.interfaces.Indexable#getIndex()
+	 */
+	@Override
+	public Integer getIndex() {
+
+		return index;
+	}
+
+	/**
+	 * Sets the index associated with this {@link SimpleIndexable}.
+	 * 
+	 * @param index
+	 *            the index for this {@link SimpleIndexable}.
+	 * @throws IllegalArgumentException
+	 *             if {@code index} is null.
+	 */
+	private void setIndex(Integer index) {
+
+		if (index == null) {
+			throw new IllegalArgumentException("Index should not be null");
+		}
+		this.index = index;
+	}
+}
