6c0a23ac44f90a3ab11337fdffb3110a40659cf8
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-04-11 14:52:28 +0000

    Created first interface.
    The nameable interface is added to the project.

---
+++ trunk/code/java/comiccollection/domain/src/main/java/org/effrafax/comiccollection/domain/model/interfaces/Nameable.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.effrafax.comiccollection.domain.model.interfaces;
+
+/**
+ * This class represants the property that an object is Nameable.
+ * 
+ * @author dvberkel
+ */
+public interface Nameable {
+
+	/**
+	 * Returns the name of this object.
+	 * 
+	 * @return
+	 */
+	public String getName();
+}
