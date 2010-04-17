3fc817fa1f3631f17e37cb349a5e4cd5b9dca6da
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-04-17 10:20:34 +0000

    Prepared InMemoryRepository
    to save albums when a comic is saved.

--- trunk/code/java/comiccollection/domain/src/main/java/org/effrafax/comiccollection/domain/service/CreationService.java
+++ trunk/code/java/comiccollection/domain/src/main/java/org/effrafax/comiccollection/domain/service/CreationService.java
@@ -5,6 +5,7 @@ package org.effrafax.comiccollection.domain.service;
 
 import org.effrafax.comiccollection.domain.factory.Factory;
 import org.effrafax.comiccollection.domain.factory.RepositoryFactory;
+import org.effrafax.comiccollection.domain.model.Comic;
 import org.effrafax.comiccollection.domain.model.Omnibus;
 import org.effrafax.comiccollection.domain.repository.Repository;
 
@@ -26,12 +27,20 @@ public class CreationService {
 		return omnibus;
 	}
 
+	/**
+	 * Adds a {@link Comic} with {@code name} to the {@link Omnibus} with
+	 * {@code omnibusId}.
+	 * 
+	 * @param omnibusId
+	 *            the id of the {@link Omnibus}.
+	 * @param name
+	 *            the name of the {@link Comic} to add.
+	 */
 	public static void addComic(Long omnibusId, String name) {
 
 		Repository repository = RepositoryFactory.getRepository();
 		Omnibus omnibus = repository.loadOmnibus(omnibusId);
 		omnibus.addComic(Factory.createComic(name));
 		repository.saveOmnibus(omnibus);
-
 	}
 }
