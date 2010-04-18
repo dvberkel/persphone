cf491674c01cecfb7d3de68e1fe12f74943c9d1d
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-04-18 10:11:07 +0000

    Made progress with JDBCRepository.
    We can now load comics.

---
+++ code/java/comiccollection/persistence/jdbc/src/main/java/org/effrafax/comiccollection/persistence/jdbc/dto/ComicDTO.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package org.effrafax.comiccollection.persistence.jdbc.dto;
+
+/**
+ * @author dvberkel
+ */
+public class ComicDTO {
+
+	private Long id;
+	private String name;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Long id) {
+
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+
+		this.name = name;
+	}
+}
