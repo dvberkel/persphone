2f06f697b21b27a03a87b7a6ca3cdf4f79c0cdba
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-06-24 20:51:31 +0000

    Added Monotonic Decreasing Sequence Iterator.
    The header says it all.

---
+++ code/java/combinatorial/src/test/java/org/effrafax/combinatorial/iterator/MonotoniDecreasingIteratorTest.java
@@ -0,0 +1,38 @@
+package org.effrafax.combinatorial.iterator;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MonotoniDecreasingIteratorTest {
+
+	@Test
+	public void testIteratorOfLengthOneHeightTwo() {
+
+		MonotonicDecreasingIterator iterator = new MonotonicDecreasingIterator(1, 2);
+
+		assertTrue(iterator.hasNext());
+		assertArrayEquals(new int[] {0}, iterator.next());
+		assertTrue(iterator.hasNext());
+		assertArrayEquals(new int[] {1}, iterator.next());
+		assertTrue(iterator.hasNext());
+		assertArrayEquals(new int[] {2}, iterator.next());
+		assertFalse(iterator.hasNext());
+	}
+
+	@Test
+	public void testIteratorOfLengthTwoHeightOne() {
+
+		MonotonicDecreasingIterator iterator = new MonotonicDecreasingIterator(2, 1);
+
+		assertTrue(iterator.hasNext());
+		assertArrayEquals(new int[] {0, 0}, iterator.next());
+		assertTrue(iterator.hasNext());
+		assertArrayEquals(new int[] {1, 0}, iterator.next());
+		assertTrue(iterator.hasNext());
+		assertArrayEquals(new int[] {1, 1}, iterator.next());
+		assertFalse(iterator.hasNext());
+	}
+}
