0eed1f513991d3ffe4911570ba7f1faa6457b356
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-07-18 07:27:49 +0000

    Created a ComicsPanel
    A comics panel is created. It holds all lists all the comics in a omnibus.

---
+++ trunk/code/java/comiccollection/ui/wicket/src/main/java/org/effrafax/comic/wicket/component/comic/ComicsPanel.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package org.effrafax.comic.wicket.component.comic;
+
+import java.util.List;
+
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.effrafax.comiccollection.domain.model.Comic;
+import org.effrafax.comiccollection.domain.model.Omnibus;
+import org.effrafax.comiccollection.domain.service.RetrievalService;
+
+/**
+ * @author dvberkel
+ * 
+ */
+public class ComicsPanel extends Panel
+{
+
+	/** */
+	private static final long serialVersionUID = 37L;
+
+	public ComicsPanel(String id, final Long omnibusId)
+	{
+		super(id);
+		Omnibus omnibus = RetrievalService.getOmnibus(omnibusId);
+		List<Comic> comics = omnibus.getComics();
+		add(new ListView<Comic>("comics", comics)
+		{
+
+			/** */
+			private static final long serialVersionUID = 37L;
+
+			@Override
+			protected void populateItem(ListItem<Comic> item)
+			{
+
+				item.add(new ComicLink("comicLink", omnibusId, item.getModel()));
+			}
+		});
+	}
+
+}
