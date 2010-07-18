272a8f408c67f3fac23880e5db30586b30e4f3c8
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-07-18 07:27:49 +0000

    Created a ComicsPanel
    A comics panel is created. It holds all lists all the comics in a omnibus.

--- code/java/comiccollection/ui/wicket/src/main/java/org/effrafax/comic/wicket/OmnibusPage.java
+++ code/java/comiccollection/ui/wicket/src/main/java/org/effrafax/comic/wicket/OmnibusPage.java
@@ -3,16 +3,10 @@
  */
 package org.effrafax.comic.wicket;
 
-import java.util.List;
-
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
-import org.effrafax.comic.wicket.component.comic.ComicLink;
-import org.effrafax.comiccollection.domain.model.Comic;
+import org.effrafax.comic.wicket.component.comic.ComicsPanel;
 import org.effrafax.comiccollection.domain.model.Omnibus;
-import org.effrafax.comiccollection.domain.service.RetrievalService;
 
 /**
  * @author dvberkel
@@ -62,23 +56,8 @@ public class OmnibusPage extends WebPage
 	 * @param omnibusId
 	 *            the id of the {@link Omnibus}.
 	 */
-	private void addComics(final Long omnibusId)
+	private void addComics(Long omnibusId)
 	{
-
-		Omnibus omnibus = RetrievalService.getOmnibus(omnibusId);
-		List<Comic> comics = omnibus.getComics();
-		add(new ListView<Comic>("comics", comics)
-		{
-
-			/** */
-			private static final long serialVersionUID = 37L;
-
-			@Override
-			protected void populateItem(ListItem<Comic> item)
-			{
-
-				item.add(new ComicLink("comicLink", omnibusId, item.getModel()));
-			}
-		});
+		add(new ComicsPanel("comics", omnibusId));
 	}
 }
