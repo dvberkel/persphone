6f845707c0a2e28b173b540543933683c6b74707
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-07-18 06:28:34 +0000

    Created a OmnibusLinkComponent
    In order to better modularize the wicket application a OmnibusLinkComponent is created. It is the first in many components.

---
+++ trunk/code/java/comiccollection/ui/wicket/src/main/java/org/effrafax/comic/wicket/component/OmnibusLink.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package org.effrafax.comic.wicket.component;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.effrafax.comic.wicket.OmnibusPage;
+import org.effrafax.comiccollection.domain.model.Omnibus;
+
+/**
+ * @author dvberkel
+ * 
+ */
+public class OmnibusLink extends Panel
+{
+
+	/** */
+	private static final long serialVersionUID = 37L;
+
+	public OmnibusLink(String id, IModel<Omnibus> omnibusModel)
+	{
+		super(id);
+		final Long omnibusId = omnibusModel.getObject().getId();
+		Link<Void> omnibusLink = new Link<Void>("omnibusLink")
+		{
+
+			/** */
+			private static final long serialVersionUID = 37L;
+
+			@Override
+			public void onClick()
+			{
+
+				setResponsePage(new OmnibusPage(omnibusId));
+			}
+		};
+		omnibusLink.add(new Label("omnibus", String.format("Omnibus %d", omnibusId)));
+
+		add(omnibusLink);
+	}
+}
