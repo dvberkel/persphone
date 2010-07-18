bff34215cd4f4fd0c42db062d205967650d79865
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-07-18 06:28:34 +0000

    Created a OmnibusLinkComponent
    In order to better modularize the wicket application a OmnibusLinkComponent is created. It is the first in many components.

--- code/java/comiccollection/ui/wicket/src/main/java/org/effrafax/comic/wicket/ComicCollectionPage.java
+++ code/java/comiccollection/ui/wicket/src/main/java/org/effrafax/comic/wicket/ComicCollectionPage.java
@@ -5,17 +5,17 @@ import java.util.List;
 
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.effrafax.comic.wicket.component.OmnibusLink;
 import org.effrafax.comiccollection.domain.model.Omnibus;
 import org.effrafax.comiccollection.domain.service.RetrievalService;
 
 /**
  * Homepage
  */
-public class ComicCollectionPage extends WebPage {
+public class ComicCollectionPage extends WebPage
+{
 
 	/** */
 	private static final long serialVersionUID = 37L;
@@ -26,9 +26,8 @@ public class ComicCollectionPage extends WebPage {
 	 * @param parameters
 	 *            Page parameters
 	 */
-	public ComicCollectionPage(final PageParameters parameters) {
-
-		add(new Label("message", "If you see this message wicket is properly configured and running"));
+	public ComicCollectionPage(final PageParameters parameters)
+	{
 
 		showOmnibusses();
 	}
@@ -36,34 +35,21 @@ public class ComicCollectionPage extends WebPage {
 	/**
 	 * Shows all the {@link Omnibus}ses.
 	 */
-	private void showOmnibusses() {
+	private void showOmnibusses()
+	{
 
 		List<Omnibus> omnibusses = new ArrayList<Omnibus>();
 		omnibusses.addAll(RetrievalService.getAllOmnibusses());
-		add(new ListView<Omnibus>("omnibusses", omnibusses) {
+		add(new ListView<Omnibus>("omnibusses", omnibusses)
+		{
 
 			/** */
 			private static final long serialVersionUID = 37L;
 
 			@Override
-			protected void populateItem(ListItem<Omnibus> item) {
-
-				Omnibus omnibus = item.getModelObject();
-				final Long omnibusId = omnibus.getId();
-				Link<Void> omnibusLink = new Link<Void>("omnibusLink") {
-
-					/** */
-					private static final long serialVersionUID = 37L;
-
-					@Override
-					public void onClick() {
-
-						setResponsePage(new OmnibusPage(omnibusId));
-					}
-				};
-				omnibusLink.add(new Label("omnibus", String.format("Omnibus %d", omnibus.getId())));
-
-				item.add(omnibusLink);
+			protected void populateItem(ListItem<Omnibus> item)
+			{
+				item.add(new OmnibusLink("omnibusLink", item.getModel()));
 			}
 		});
 	}
