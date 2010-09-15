a94143380519628e144bc59fbb77d9372bcb6af2
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-09-15 10:16:41 +0000

    Solution to first two tests

--- code/python/tdd/tdd.py
+++ code/python/tdd/tdd.py
@@ -3,10 +3,30 @@ class Product():
 		self.name = name
 		self.price = price
 
+class ShoppingCart():
+	content = []
+	price = 0
+	def __init__(self,product):
+		self.add_product(product,1)
+	
+	def add_product(self,product, quantity):
+		for i in range(quantity):
+			self.content.append(product)
+			self.price += product.price
+			
+
 def test1():
 	product = Product("Sunflower", 2)
 	assert product.name == "Sunflower"
 	assert product.price == 2
 
+def test2():
+	flower = Product("Sunflower", 2)
+	vase = Product("Vase", 13)
+	shopping_cart = ShoppingCart(vase)
+	shopping_cart.add_product(flower, 6)
+	assert shopping_cart.price == 25, "Shopping cart price not 25!"
+
 if __name__ == '__main__':
 	test1()
+	test2()
