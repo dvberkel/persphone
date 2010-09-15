bee2c2b341ab26d35775d5e15fcfb05f654ac55c
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-09-15 10:38:28 +0000

    Solution to first 4 tests.

--- code/python/tdd/tdd.py
+++ code/python/tdd/tdd.py
@@ -6,12 +6,16 @@ class Product():
 class ShoppingCart():
 	products = []
 	price = 0
-	def __init__(self,products):
+	def __init__(self,products,*quantities):
 		if not type(products) is type([]):
 			products = [products]
 		
+		quantity = 1
+		if len(quantities) != 0:
+			quantity = quantities[0]
+		
 		for product in products:
-			self.add_product(product,1)
+			self.add_product(product,quantity)
 		
 
 	def add_product(self,product, quantity):
@@ -20,26 +24,33 @@ class ShoppingCart():
 			self.price += product.price
 			
 
-def test1():
+def test0():
 	product = Product("Sunflower", 2)
 	assert product.name == "Sunflower"
 	assert product.price == 2
 
-def test2():
+def test1():
 	flower = Product("Sunflower", 2)
 	vase = Product("Vase", 13)
 	shopping_cart = ShoppingCart(vase)
 	shopping_cart.add_product(flower, 6)
 	assert shopping_cart.price == 25, "Shopping cart price not 25!"
 
-def test3():
+def test2():
 	flower = Product("Sunflower", 2)
 	vase = Product("Vase", 13)
 	shopping_cart = ShoppingCart([flower, vase])
 	assert flower in shopping_cart.products, "flower not in products!"
 	assert vase in shopping_cart.products, "vase not in products!"
 
+def test3():
+	flower = Product("Sunflower", 2)
+	shopping_cart = ShoppingCart(flower, 6)
+	assert shopping_cart.price == 12, "price not 12!"
+
+
 if __name__ == '__main__':
+	test0()
 	test1()
 	test2()
 	test3()
