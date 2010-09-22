516fd87aab6695e843fbeb8803f99f5efdb6cdc0
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-09-22 06:55:18 +0000

    Advanced the Tic-Tac-Toe example
    The train is very productive. Get an insight that the position is not 
    necessary.

--- code/python/tic-tac-toe/Cell.py
+++ code/python/tic-tac-toe/Cell.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python
 
-from PieceFactory import PieceFactory
+import PieceFactory
+from Observable import Observable
+import Observer
 
-class Cell():
-	def __init__(self,*piece):
-		if (len(piece) > 0):
-			self.piece = piece[0]
-		else:
-			self.piece = PieceFactory.empty
+class Cell(Observable):
+	def __init__(self):
+		Observable.__init__(self)
+		self.piece = PieceFactory.empty
 	
 	def playable(self):
 		return self.piece == PieceFactory.empty
@@ -15,15 +15,19 @@ class Cell():
 	def place(self,piece):
 		assert self.playable()
 		self.piece = piece
+		self.notifyObservers()
 		
 if __name__ == '__main__':
 	assert Cell()
-	assert Cell(PieceFactory.cross)
 	
 	cell = Cell()
 	assert cell.playable()
 	cell.place(PieceFactory.cross)
 	assert cell.piece == PieceFactory.cross
+	assert not cell.playable()
 	
-	cell = Cell(PieceFactory.cross)
-	assert not cell.playable
+	observer = Observer.TestObserver()
+	cell = Cell()
+	cell.subscribe(observer)
+	cell.place(PieceFactory.cross)
+	assert observer.count == 1
--- code/python/tic-tac-toe/PieceFactory.py
+++ code/python/tic-tac-toe/PieceFactory.py
@@ -12,18 +12,17 @@ class Cross(Piece):
 class Empty(Piece):
 	pass
 
-class PieceFactory():
-	nought = Nought()
-	cross = Cross()
-	empty = Empty()
+nought = Nought()
+cross = Cross()
+empty = Empty()
 	
 if __name__ == '__main__':
-	assert PieceFactory.nought == PieceFactory.nought
-	assert PieceFactory.nought != PieceFactory.cross
-	assert PieceFactory.nought != PieceFactory.empty
-	assert PieceFactory.cross == PieceFactory.cross
-	assert PieceFactory.cross != PieceFactory.nought
-	assert PieceFactory.cross != PieceFactory.empty
-	assert PieceFactory.empty == PieceFactory.empty
-	assert PieceFactory.empty != PieceFactory.nought
-	assert PieceFactory.empty != PieceFactory.cross
+	assert nought == nought
+	assert nought != cross
+	assert nought != empty
+	assert cross == cross
+	assert cross != nought
+	assert cross != empty
+	assert empty == empty
+	assert empty != nought
+	assert empty != cross
