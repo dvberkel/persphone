e3da263591de186c0776ef5600dbc38a833488f9
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-09-29 11:40:25 +0000

    Uncommitted code
    This piece of code was accidently not committed.

---
+++ code/python/tic-tac-toe/view/representation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from domain.piece import piecefactory
+from domain.game import Game
+
+_symbolFor = {piecefactory.nought:'O', piecefactory.cross:'X', piecefactory.empty:'_'}
+
+def present(game):
+	return reduce((lambda x,y: x + y),map((lambda y: _symbolFor[y]),map((lambda x: x.piece),game.cells())))
+
+
+if __name__ == '__main__':
+	game = Game(piecefactory.nought)
+	assert present(game) == '_________'
+
