da02ff9b666fea8f211e011610e94cd7f01c249e
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-10-02 17:17:28 +0000

    Introduced navigation
    It is now possible to navigate throught the pages in a introductory manner.
    
    The next step will be the introduction of models.

--- code/python/tic-tac-toe/view/appengine/game/views.py
+++ code/python/tic-tac-toe/view/appengine/game/views.py
@@ -7,10 +7,12 @@ def overview(request):
 	return render_to_response('game/overview.html', {})
 
 
-def history(request,gameId):
+def history(request,gameId,ply=10):
 	template = loader.get_template('game/history.html')
 	context = Context({
 		'gameId': gameId,
+		'prevPly': int(ply)-1,
+		'nextPly': int(ply)+1
 	})
 	return HttpResponse(template.render(context))
 
