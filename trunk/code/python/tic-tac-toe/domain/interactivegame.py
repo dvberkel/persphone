5d4a99f957c7a8f8be6955a31b0718b71ed364fd
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-10-02 18:08:23 +0000

    Created a ReplayableGame
    This game allows to replay itself according to the representations given.

--- trunk/code/python/tic-tac-toe/domain/interactivegame.py
+++ trunk/code/python/tic-tac-toe/domain/interactivegame.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-from game import Game
+from replayablegame import ReplayableGame
 from pattern.notify.observable import Observable
 from pattern.factory import piecefactory
 from domain.player import RandomPlayer
 
-class InteractiveGame(Game,Observable):
+class InteractiveGame(ReplayableGame,Observable):
 	def __init__(self,noughtPlayer,crossPlayer):
-		Game.__init__(self,piecefactory.nought)
+		ReplayableGame.__init__(self,piecefactory.nought)
 		Observable.__init__(self)
 		self._players = {True:noughtPlayer, False:crossPlayer}
 		self._currentKey = True
