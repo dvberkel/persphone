e67befbcd174cdde2f37a1fe43859970e24b1225
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-10-02 19:38:13 +0000

    Created First Model
    A GameModel and PlyModel are created and the GameModel is used.

--- trunk/code/python/tic-tac-toe/view/appengine/game/views.py
+++ trunk/code/python/tic-tac-toe/view/appengine/game/views.py
@@ -3,14 +3,25 @@ from django.http import HttpResponse
 from django.template import Context, loader
 from django.shortcuts import render_to_response
 
+import uuid
+from google.appengine.api import users
+from view.appengine.game.models import GameModel
+
+def new(request):
+	gameModel = GameModel(owner=users.get_current_user(),uuid=str(uuid.uuid4()),over=False)
+	gameModel.put()
+	context = Context({
+		'game_uuid': gameModel.uuid
+	})
+	return render_to_response('game/history.html', context)
+
 def overview(request):
 	return render_to_response('game/overview.html', {})
 
-
-def history(request,gameId,ply=10):
+def history(request,game_uuid,ply=10):
 	template = loader.get_template('game/history.html')
 	context = Context({
-		'gameId': gameId,
+		'game_uuid': game_uuid,
 		'prevPly': int(ply)-1,
 		'nextPly': int(ply)+1
 	})
