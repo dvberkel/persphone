015ed8da669deea964d11659cac79550bf546f2f
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-10-06 14:09:21 +0000

    Removed Warnings
    Eclipse detected a lot of unreferenced imports. These are removed in this commit.

--- code/python/tic-tac-toe/view/appengine/base/views.py
+++ code/python/tic-tac-toe/view/appengine/base/views.py
@@ -1,6 +1,3 @@
-from django.http import HttpRequest
-from django.http import HttpResponse
-from django.template import Context, loader
 from django.shortcuts import render_to_response
 
 def login(request):
