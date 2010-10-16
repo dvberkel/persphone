da165a36edc90836372c939c288bc60a64e09b9e
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-10-16 08:41:05 +0000

    Added a sign out link
    A link to sign out of the application is added.

--- code/python/tic-tac-toe/view/appengine/base/views.py
+++ code/python/tic-tac-toe/view/appengine/base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response
+from google.appengine.api import users
 
 def login(request):
-	return render_to_response('base.html', {})
+	return render_to_response('base.html', {'signout' : users.create_logout_url(request.path)})
