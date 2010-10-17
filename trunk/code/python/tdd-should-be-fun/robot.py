72e252923435c0d4655ab710f80ce4493da967ce
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-10-17 10:24:00 +0000

    Added test
    The definition of location changed because of ease of use.

--- trunk/code/python/tdd-should-be-fun/robot.py
+++ trunk/code/python/tdd-should-be-fun/robot.py
@@ -1,12 +1,25 @@
 class Robot():
 	def __init__(self):
-		self.location = (0,0)
+		self.location = [0,0]
+	
+	def forward(self):
+		self.location[0] += 1
+		self.location[1] += 0
+		return self
 
 def test0():
 	robot = Robot()
 	
-	assert robot.location == (0,0)
+	assert robot.location == [0,0]
+
+def test1():
+	robot = Robot()
 	
+	robot.forward().forward()
+	
+	assert robot.location == [2,0]
+
 
 if __name__ == '__main__':
 	test0()
+	test1()
