232dac76be8928bd27db5ec2004098d60bc1f2cc
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-10-17 10:28:50 +0000

    Added test
    A new test is added.

--- code/python/tdd-should-be-fun/robot.py
+++ code/python/tdd-should-be-fun/robot.py
@@ -1,10 +1,19 @@
 class Robot():
 	def __init__(self):
 		self.location = [0,0]
+		self.heading = [1,0]
 	
 	def forward(self):
-		self.location[0] += 1
-		self.location[1] += 0
+		self.location[0] += self.heading[0]
+		self.location[1] += self.heading[1]
+		return self
+	
+	def left(self):
+		self.heading = [-self.heading[1],self.heading[0]]
+		return self
+	
+	def right(self):
+		self.heading = [self.heading[1],-self.heading[0]]
 		return self
 
 def test0():
@@ -19,7 +28,17 @@ def test1():
 	
 	assert robot.location == [2,0]
 
+def test2():
+	robot = Robot()
+	
+	robot.left().forward()
+	assert robot.location == [0,1]
+
+	robot.right().forward()
+	assert robot.location == [1,1]
+
 
 if __name__ == '__main__':
 	test0()
 	test1()
+	test2()
