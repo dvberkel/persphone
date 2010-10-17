9de445137d930424000b773b2f5cd51104d239a9
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-10-17 19:48:00 +0000

    Restructed code
    The code is restructured in order to allow the next test to pass.

--- code/python/tdd-should-be-fun/robot.py
+++ code/python/tdd-should-be-fun/robot.py
@@ -20,13 +20,36 @@ class Robot():
 		self.program = program
 	
 	def execute(self):
-		for command in self.program:
-			if (command == 'F'):
-				self.forward()
-			if (command == 'L'):
-				self.left()
-			if (command == 'R'):
-				self.right();
+		tokenizer = Tokenizer(self.program)
+		token = tokenizer.next()
+		while (token):
+			if (token.concept == 'MOVE'):
+				if (token.value == 'F'):
+					self.forward()
+				if (token.value == 'L'):
+					self.left()
+				if (token.value == 'R'):
+					self.right()
+			token = tokenizer.next()
+
+class Tokenizer():
+	def __init__(self, program):
+		self.program = program
+		self. index = 0
+	
+	def next(self):
+		if (self.index < len(self.program)):
+			if (self.program[self.index] in 'FLR'):
+				value = self.program[self.index]
+				self.index += 1
+				return Token('MOVE',value)
+		else:
+			return None	
+
+class Token():
+	def __init__(self,concept,value):
+		self.concept = concept
+		self.value = value
 
 def test0():
 	robot = Robot()
@@ -56,6 +79,14 @@ def test3():
 	robot.execute()
 	
 	assert robot.location == [2,1]
+
+def test4():
+	robot = Robot()
+	
+	robot.load('5FL3F')
+	robot.execute()
+	
+	assert robot.location == [5,3]
 
 
 if __name__ == '__main__':
