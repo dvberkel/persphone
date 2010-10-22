a8f312e5150de27e601e8d3c03f5a2dfa97953d2
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-10-22 08:29:55 +0000

    Added test
    The robot scenario is polished and almost ready now.

--- trunk/code/python/tdd-should-be-fun/robot.py
+++ trunk/code/python/tdd-should-be-fun/robot.py
@@ -44,6 +44,11 @@ class Robot():
 			nextToken = tokenizer.next()
 			for number in range(token.value):
 				self.executeToken(tokenizer,nextToken)
+		if (token.concept == 'MARK'):
+			if (token.value == 'M'):
+				self.pushMark()
+			if (token.value == 'P'):
+				self.popMark().execute()
 	
 	def plan(self,location):
 		program = ''
@@ -65,13 +70,15 @@ class Robot():
 		elif delta[1] > 0:
 			program += 'L%dFR' % abs(delta[1])
 		
-		self.load(program)
+		return self.load(program)		
 	
 	def pushMark(self):
-		self.marks.append(self.location)
+		self.marks.append(self.location[:])
+		return self
 	
 	def popMark(self):
 		self.plan(self.marks.pop())
+		return self
 	
 class Tokenizer():
 	def __init__(self, program):
@@ -89,6 +96,10 @@ class Tokenizer():
 				while (self.program[self.index] in '0123456789'):
 					self.index += 1
 				return Token('INT',int(self.program[start:self.index]))
+			if (self.program[self.index] in 'MP'):
+				value = self.program[self.index]
+				self.index += 1
+				return Token('MARK',value)
 		else:
 			return None	
 
@@ -99,19 +110,15 @@ class Token():
 
 def test0():
 	robot = Robot()
-	
 	assert robot.location == [0,0]
 
 def test1():
 	robot = Robot()
-	
 	robot.forward().forward()
-	
 	assert robot.location == [2,0]
 
 def test2():
 	robot = Robot()
-	
 	robot.left().forward()
 	assert robot.location == [0,1]
 
@@ -120,52 +127,59 @@ def test2():
 
 def test2a():
 	robot = Robot()
-	
 	robot.forward(3)
 	assert robot.location == [3,0]
 
 	robot.left(3).forward().right(2).forward(2)
 	assert robot.location == [3,1]
 	
-	
 def test3():
 	robot = Robot()
-	
 	robot.load('FLFRF')
 	robot.execute()
-	
 	assert robot.location == [2,1]
 
 def test4():
 	robot = Robot()
-	
 	robot.load('5FL3F').execute()
-	
 	assert robot.location == [5,3]
 
 def test5():
 	robot = Robot()
-	
 	robot.plan([8,5])
 	assert robot.location == [0,0] # Robots can not teleport!
 	robot.execute()
-	
 	assert robot.location == [8,5]
 	
-	robot.plan([2,1])
-	robot.execute()
+	robot.forward()
+	assert robot.location == [9,5]
 	
+	robot.plan([2,1]).execute()
 	assert robot.location == [2,1]
+
 
 def test6():
 	robot = Robot()
-	
 	robot.pushMark()
 	robot.forward(8).left().forward(5)
-	robot.pushMark()
-	robot.forward(8).left().forward(5)
+	robot.pushMark().forward(3).left().forward(5)
+	assert robot.location == [3,8]
 	
-	assert robot.location == [3,13]
+	robot.popMark().execute()
+	assert robot.location == [8,5]
+
+def test7():
+	robot = Robot()
+	robot.load('5FM8FL3F').execute()
+	assert robot.location == [13,3]
+	
+	robot.popMark().execute()
+	assert robot.location == [5,0]
+
+def test8():
+	robot = Robot()
+	robot.load('M21FR34FPFLF').execute()
+	assert robot.location == [1,1]
 
 if __name__ == '__main__':
 	test0()
@@ -176,3 +190,5 @@ if __name__ == '__main__':
 	test4()
 	test5()
 	test6()
+	test7()
+	test8()
