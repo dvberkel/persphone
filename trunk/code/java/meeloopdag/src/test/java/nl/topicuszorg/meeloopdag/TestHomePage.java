c4ea59bab24ebc0131b24f73bd2f25503405975b
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-12-05 20:50:57 +0000

    Fixed Wicket Test setup
    Modernized the current wicket test. It used slf4j 1.5.8 and this isn't the
    latest version.

--- trunk/code/java/meeloopdag/src/test/java/nl/topicuszorg/meeloopdag/TestHomePage.java
+++ trunk/code/java/meeloopdag/src/test/java/nl/topicuszorg/meeloopdag/TestHomePage.java
@@ -1,29 +1,32 @@
 package nl.topicuszorg.meeloopdag;
 
-import junit.framework.TestCase;
 import org.apache.wicket.util.tester.WicketTester;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Simple test using the WicketTester
  */
-public class TestHomePage extends TestCase
+public class TestHomePage
 {
 	private WicketTester tester;
 
-	public void setUp()
+	@Before
+	public void setUpTester()
 	{
 		tester = new WicketTester();
 	}
 
+	@Test
 	public void testRenderMyPage()
 	{
-		//start and render the test page
+		// start and render the test page
 		tester.startPage(HomePage.class);
 
-		//assert rendered page class
+		// assert rendered page class
 		tester.assertRenderedPage(HomePage.class);
 
-		//assert rendered label component
+		// assert rendered label component
 		tester.assertLabel("message", "If you see this message wicket is properly configured and running");
 	}
 }
