fad1e1392483ede4d0b70e2648998e27426b1fae
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-12-06 08:26:21 +0000

    Added pages and Panel
    Various pages and panels are created in order to fascilitate the new
    BMI calculator.

---
+++ code/java/meeloopdag/src/main/java/nl/topicuszorg/meeloopdag/web/bmi/BMIPanel.java
@@ -0,0 +1,26 @@
+package nl.topicuszorg.meeloopdag.web.bmi;
+
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class BMIPanel extends Panel
+{
+	private static final long serialVersionUID = 37L;
+
+	public BMIPanel(String id)
+	{
+		super(id);
+		add(new BMIForm("bmiform"));
+	}
+
+	class BMIForm extends Form<Void>
+	{
+		private static final long serialVersionUID = 37L;
+
+		public BMIForm(String id)
+		{
+			super(id);
+		}
+
+	}
+}
