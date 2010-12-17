c3407c8be146d64cc8ee894664c41ace697e71ad
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2010-12-17 11:32:29 +0000

    Extended input
    The input is extended in order to easily allow different inputControle.

--- trunk/code/java/meeloopdag/src/main/java/nl/topicuszorg/meeloopdag/web/bmi/invoer/converter/controle/GebrokenGetalControle.java
+++ trunk/code/java/meeloopdag/src/main/java/nl/topicuszorg/meeloopdag/web/bmi/invoer/converter/controle/GebrokenGetalControle.java
@@ -1,12 +1,11 @@
 package nl.topicuszorg.meeloopdag.web.bmi.invoer.converter.controle;
 
-public class GeheelGetalControle implements InputControle
+public class GebrokenGetalControle implements InputControle
 {
 
 	@Override
 	public boolean correcteInput(String input)
 	{
-		return input.matches("0|-?[1-9]\\d*");
+		return input.matches("(0|-?[1-9]\\d*)(\\.\\d*)?||\\.\\d+");
 	}
-
 }
--- trunk/code/java/meeloopdag/src/test/java/nl/topicuszorg/meeloopdag/web/bmi/invoer/converter/controle/GeheelGetalControleTest.java
+++ trunk/code/java/meeloopdag/src/test/java/nl/topicuszorg/meeloopdag/web/bmi/invoer/converter/controle/GeheelGetalControleTest.java
@@ -31,6 +31,25 @@ public class GeheelGetalControleTest
 	}
 
 	@Test
+	public void geheelGetalControleVindNegatieveGeheleGetallenCorrect()
+	{
+		assertTrue(controle.correcteInput("-37"));
+		assertTrue(controle.correcteInput("-51"));
+	}
+
+	@Test
+	public void geheelGetalControleVindNulCorrect()
+	{
+		assertTrue(controle.correcteInput("0"));
+	}
+
+	@Test
+	public void geheelGetalControleVindMinNulNietCorrect()
+	{
+		assertFalse(controle.correcteInput("-0"));
+	}
+
+	@Test
 	public void geheelGetalControleVindKommaGetallenNietCorrect()
 	{
 		assertFalse(controle.correcteInput("3.7"));
