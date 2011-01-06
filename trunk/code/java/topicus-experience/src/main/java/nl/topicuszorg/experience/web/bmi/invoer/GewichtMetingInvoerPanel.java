07b18e1dcb0a11ed6700d80d07f144431ae45142
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2011-01-06 20:42:34 +0000

    AbstractMetingInvoerPanel introduced
    The abstractMetingInvoerPanel is added to generalize the panel.

--- trunk/code/java/topicus-experience/src/main/java/nl/topicuszorg/experience/web/bmi/invoer/GewichtMetingInvoerPanel.java
+++ trunk/code/java/topicus-experience/src/main/java/nl/topicuszorg/experience/web/bmi/invoer/GewichtMetingInvoerPanel.java
@@ -1,23 +1,11 @@
 package nl.topicuszorg.experience.web.bmi.invoer;
 
-import static nl.topicuszorg.experience.domain.meting.Meting.meting;
 import static nl.topicuszorg.experience.domain.meting.eenheid.Eenheid.gram;
-import static nl.topicuszorg.experience.domain.meting.meetwaarde.Meetwaarde.meetwaarde;
-import nl.topicuszorg.experience.domain.meting.Meting;
 import nl.topicuszorg.experience.domain.meting.eenheid.Gram;
-import nl.topicuszorg.experience.domain.meting.meetwaarde.Meetwaarde;
-import nl.topicuszorg.experience.web.bmi.invoer.meetwaarde.MeetwaardeInvoerVeld;
 
-import org.apache.wicket.markup.html.form.FormComponentPanel;
-import org.apache.wicket.model.PropertyModel;
-
-public class GewichtMetingInvoerPanel extends FormComponentPanel<Meting<Gram>>
+public class GewichtMetingInvoerPanel extends AbstractMetingInvoerPanel<Gram>
 {
 	private static final long serialVersionUID = 37L;
-
-	private Meetwaarde meetwaarde = meetwaarde(80000);
-
-	private MeetwaardeInvoerVeld meetwaardeInvoerVeld;
 
 	public GewichtMetingInvoerPanel(String id)
 	{
@@ -25,17 +13,9 @@ public class GewichtMetingInvoerPanel extends FormComponentPanel<Meting<Gram>>
 		init();
 	}
 
-	private void init()
-	{
-		setRequired(true);
-		meetwaardeInvoerVeld = new MeetwaardeInvoerVeld("meetwaarde", new PropertyModel<Meetwaarde>(this, "meetwaarde"));
-		add(meetwaardeInvoerVeld);
-	}
-
 	@Override
-	protected void convertInput()
+	protected Gram getEenheid()
 	{
-		setConvertedInput(meting(meetwaardeInvoerVeld.getConvertedInput(), gram()));
+		return gram();
 	}
-
 }
