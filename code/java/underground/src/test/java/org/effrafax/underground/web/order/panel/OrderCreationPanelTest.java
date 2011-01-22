8b0b4297446877172c272ada6fb1fe937c467a3c
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2011-01-22 14:43:44 +0000

    Abstract Underground Wicket Test

--- code/java/underground/src/test/java/org/effrafax/underground/web/order/panel/OrderCreationPanelTest.java
+++ code/java/underground/src/test/java/org/effrafax/underground/web/order/panel/OrderCreationPanelTest.java
@@ -1,40 +1,24 @@
 package org.effrafax.underground.web.order.panel;
 
-import static org.junit.Assert.assertNotNull;
 
-import org.apache.wicket.util.tester.WicketTester;
+import org.effrafax.underground.util.test.UndergroundWicketTest;
 import org.effrafax.underground.web.order.panel.component.OrderInputComponent;
-import org.junit.Before;
 import org.junit.Test;
 
-public class OrderCreationPanelTest
+public class OrderCreationPanelTest extends UndergroundWicketTest
 {
-	private WicketTester tester;
-
-	@Before
-	public void createWicketTester()
-	{
-		tester = new WicketTester();
-	}
-
-	@Test
-	public void shouldCreateAWicketTester()
-	{
-		assertNotNull(tester);
-	}
-
 	@Test
 	public void shouldRenderCorrectly()
 	{
-		tester.startPanel(OrderCreationPanel.class);
-		tester.assertNoErrorMessage();
-		tester.assertNoInfoMessage();
+		getTester().startPanel(OrderCreationPanel.class);
+		getTester().assertNoErrorMessage();
+		getTester().assertNoInfoMessage();
 	}
 
 	@Test
 	public void shouldHaveAnOrderInputComponent()
 	{
-		tester.startPanel(OrderCreationPanel.class);
-		tester.assertComponent("panel:orderInputComponent", OrderInputComponent.class);
+		getTester().startPanel(OrderCreationPanel.class);
+		getTester().assertComponent("panel:orderInputComponent", OrderInputComponent.class);
 	}
 }
