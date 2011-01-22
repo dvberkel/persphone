c4349ebc48ce1231d7eaf0225258a57df6df54b3
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2011-01-22 07:49:55 +0000

    Changed into a GAE project
    By changing this files this project is ready to be deployed to Google App Engine.

--- trunk/code/java/underground/src/main/java/org/effrafax/underground/WicketApplication.java
+++ trunk/code/java/underground/src/main/java/org/effrafax/underground/WicketApplication.java
@@ -1,27 +1,36 @@
 package org.effrafax.underground;
 
+import org.apache.wicket.protocol.http.HttpSessionStore;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.session.ISessionStore;
 
 /**
- * Application object for your web application. If you want to run this application without deploying, run the Start class.
+ * Application object for your web application. If you want to run this application without deploying, run the Start
+ * class.
  * 
  * @see org.effrafax.underground.Start#main(String[])
  */
 public class WicketApplication extends WebApplication
-{    
-    /**
-     * Constructor
-     */
+{
+	/**
+	 * Constructor
+	 */
 	public WicketApplication()
 	{
 	}
-	
+
 	/**
 	 * @see org.apache.wicket.Application#getHomePage()
 	 */
 	public Class<HomePage> getHomePage()
 	{
 		return HomePage.class;
+	}
+
+	@Override
+	protected ISessionStore newSessionStore()
+	{
+		return new HttpSessionStore(this);
 	}
 
 }
