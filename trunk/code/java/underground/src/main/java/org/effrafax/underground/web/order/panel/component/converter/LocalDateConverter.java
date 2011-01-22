53508052acf05a1d33c6cae7195796bbe9005c40
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2011-01-22 14:36:08 +0000

    Added A LocalDateConverter
    This convert converts textual representations of date to an actial localDateInstance.

---
+++ trunk/code/java/underground/src/main/java/org/effrafax/underground/web/order/panel/component/converter/LocalDateConverter.java
@@ -0,0 +1,36 @@
+package org.effrafax.underground.web.order.panel.component.converter;
+
+import java.util.Locale;
+
+import org.apache.wicket.util.convert.converters.AbstractConverter;
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.DateTimeFormatterBuilder;
+
+public class LocalDateConverter extends AbstractConverter
+{
+	private static final long serialVersionUID = 37L;
+
+	private DateTimeFormatter dateTimeFormatter;
+
+	public LocalDateConverter()
+	{
+		DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder();
+		builder.appendDayOfMonth(1).appendLiteral("-");
+		builder.appendMonthOfYear(1).appendLiteral("-");
+		builder.appendYear(4, 4);
+		dateTimeFormatter = builder.toFormatter();
+	}
+
+	@Override
+	protected Class<?> getTargetType()
+	{
+		return LocalDate.class;
+	}
+
+	@Override
+	public Object convertToObject(String value, Locale locale)
+	{
+		return dateTimeFormatter.parseDateTime(value).toLocalDate();
+	}
+}
---
+++ trunk/code/java/underground/src/test/java/org/effrafax/underground/web/order/panel/converter/LocalDateConverterTest.java
@@ -0,0 +1,33 @@
+package org.effrafax.underground.web.order.panel.converter;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.effrafax.underground.web.order.panel.component.converter.LocalDateConverter;
+import org.joda.time.LocalDate;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LocalDateConverterTest
+{
+	private LocalDateConverter localDateConverter;
+
+	@Before
+	public void createLocalDateConverter()
+	{
+		localDateConverter = new LocalDateConverter();
+	}
+
+	@Test
+	public void shouldBeAbleToCreateALocalDateConverter()
+	{
+		assertNotNull(localDateConverter);
+	}
+
+	@Test
+	public void shouldParseADateCorrectly()
+	{
+		LocalDate expectLocalDate = new LocalDate(2011, 1, 22);
+		assertEquals(expectLocalDate, localDateConverter.convertToObject("22-1-2011", null));
+	}
+}
