087f52419a00ec0c7762d3318b6e1b057109a066
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2011-01-22 08:42:10 +0000

    Few Test Iterations for Order
    This commit signals the end of the first test iterations for Order.

--- trunk/code/java/underground/src/main/java/org/effrafax/underground/domain/Order.java
+++ trunk/code/java/underground/src/main/java/org/effrafax/underground/domain/Order.java
@@ -1,11 +1,34 @@
 package org.effrafax.underground.domain;
 
+import org.joda.time.LocalDate;
+
 public class Order
 {
 
+	private final LocalDate orderDate;
+
+	public static Order create(LocalDate orderDate)
+	{
+		if (orderDate == null)
+		{
+			throw new IllegalArgumentException("orderDate should not be null.");
+		}
+		return new Order(orderDate);
+	}
+
 	public static Order create()
 	{
-		return new Order();
+		return create(new LocalDate());
+	}
+
+	private Order(LocalDate orderDate)
+	{
+		this.orderDate = orderDate;
+	}
+
+	public Object getOrderDate()
+	{
+		return orderDate;
 	}
 
 }
--- trunk/code/java/underground/src/test/java/org/effrafax/underground/domain/OrderTest.java
+++ trunk/code/java/underground/src/test/java/org/effrafax/underground/domain/OrderTest.java
@@ -1,7 +1,9 @@
 package org.effrafax.underground.domain;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import org.joda.time.LocalDate;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,5 +21,31 @@ public class OrderTest
 	public void shouldBePossibleToConstructAnOrder()
 	{
 		assertNotNull(order);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldFailWhenConstructedWithANullLocalDate()
+	{
+		Order.create(null);
+	}
+
+	@Test
+	public void shouldBePossibleToConstructAnOrderForACertainDate()
+	{
+		assertNotNull(Order.create(new LocalDate(2011, 1, 22)));
+	}
+
+	@Test
+	public void shouldReturnTodayAsOrderDateWhenConstructedWithoutExpliciteOrderDate()
+	{
+		assertEquals(new LocalDate(), Order.create().getOrderDate());
+	}
+
+	@Test
+	public void shouldReturnExpectedLocalDateAsOrderDateWhenConstructedWithExpliciteOrderDate()
+	{
+
+		LocalDate expectedLocalDate = new LocalDate(2011, 5, 12);
+		assertEquals(expectedLocalDate, Order.create(expectedLocalDate).getOrderDate());
 	}
 }
