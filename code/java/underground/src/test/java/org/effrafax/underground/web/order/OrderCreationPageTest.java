6ee333b744fb7ac32e633136633842e2c8172b75
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2011-01-22 08:18:45 +0000

    Added OrderCreationPage
    This Page will make it possible to create an order.

---
+++ code/java/underground/src/test/java/org/effrafax/underground/web/order/OrderCreationPageTest.java
@@ -0,0 +1,32 @@
+package org.effrafax.underground.web.order;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.apache.wicket.util.tester.WicketTester;
+import org.effrafax.underground.web.order.OrderCreationPage;
+import org.junit.Before;
+import org.junit.Test;
+
+public class OrderCreationPageTest
+{
+	private WicketTester tester;
+
+	@Before
+	public void createWicketTester()
+	{
+		tester = new WicketTester();
+	}
+
+	@Test
+	public void shouldCreateAWicketTester()
+	{
+		assertNotNull(tester);
+	}
+
+	@Test
+	public void shouldRenderCorrectly()
+	{
+		tester.startPage(OrderCreationPage.class);
+		tester.assertRenderedPage(OrderCreationPage.class);
+	}
+}
