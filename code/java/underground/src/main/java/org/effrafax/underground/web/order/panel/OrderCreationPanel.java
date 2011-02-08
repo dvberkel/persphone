c033629378f4428ac0b574a465233b0f5de7b950
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2011-02-08 15:56:11 +0000

    OrderInputForm introduced.

--- code/java/underground/src/main/java/org/effrafax/underground/web/order/panel/OrderCreationPanel.java
+++ code/java/underground/src/main/java/org/effrafax/underground/web/order/panel/OrderCreationPanel.java
@@ -1,20 +1,16 @@
 package org.effrafax.underground.web.order.panel;
 
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.PropertyModel;
-import org.effrafax.underground.domain.Order;
-import org.effrafax.underground.web.order.panel.component.OrderInputComponent;
+import org.effrafax.underground.web.order.panel.form.OrderCreationForm;
 
 public class OrderCreationPanel extends Panel
 {
 	private static final long serialVersionUID = 37L;
 
-	private Order order = null;
-
 	public OrderCreationPanel(String id)
 	{
 		super(id);
-		add(new OrderInputComponent("orderInputComponent", new PropertyModel<Order>(this, "order")));
+		add(new OrderCreationForm("orderCreationForm"));
 	}
 
 }
--- code/java/underground/src/test/java/org/effrafax/underground/web/order/panel/OrderCreationPanelTest.java
+++ code/java/underground/src/test/java/org/effrafax/underground/web/order/panel/OrderCreationPanelTest.java
@@ -1,7 +1,7 @@
 package org.effrafax.underground.web.order.panel;
 
 import org.effrafax.underground.util.test.UndergroundWicketTest;
-import org.effrafax.underground.web.order.panel.component.OrderInputComponent;
+import org.effrafax.underground.web.order.panel.form.OrderCreationForm;
 import org.junit.Test;
 
 public class OrderCreationPanelTest extends UndergroundWicketTest
@@ -15,9 +15,9 @@ public class OrderCreationPanelTest extends UndergroundWicketTest
 	}
 
 	@Test
-	public void shouldHaveAnOrderInputComponent()
+	public void shouldHaveAnOrderInputForm()
 	{
 		getTester().startPanel(OrderCreationPanel.class);
-		getTester().assertComponent("panel:orderInputComponent", OrderInputComponent.class);
+		getTester().assertComponent("panel:orderCreationForm", OrderCreationForm.class);
 	}
 }
