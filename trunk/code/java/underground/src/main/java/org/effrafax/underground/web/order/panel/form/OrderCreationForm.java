e560dec976114db9c0a8ffad775d086d2934b257
daan.v.berkel.1980 <daan.v.berkel.1980@cb6df1ea-5776-11de-8dc1-e798e446b60a>
2011-02-08 15:56:11 +0000

    OrderInputForm introduced.

--- trunk/code/java/underground/src/main/java/org/effrafax/underground/web/order/panel/form/OrderCreationForm.java
+++ trunk/code/java/underground/src/main/java/org/effrafax/underground/web/order/panel/form/OrderCreationForm.java
@@ -1,15 +1,26 @@
 package org.effrafax.underground.web.order.panel.form;
 
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.PropertyModel;
 import org.effrafax.underground.domain.Order;
+import org.effrafax.underground.web.order.panel.component.OrderInputComponent;
 
 public class OrderCreationForm extends Form<Order>
 {
 	private static final long serialVersionUID = 37L;
 
+	private Order order = null;
+
 	public OrderCreationForm(String id)
 	{
 		super(id);
+	}
+
+	@Override
+	protected void onInitialize()
+	{
+		super.onInitialize();
+		add(new OrderInputComponent("orderInputComponent", new PropertyModel<Order>(this, "order")));
 	}
 
 }
